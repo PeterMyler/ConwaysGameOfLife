93205f5bbaa5e0e4a90d51cdc81eb6e6352b065a
PeterTheCool <petermyler100@gmail.com>
2024-03-23 20:08:13 +0000

    added position wrapping

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pygame as pg
 import numpy as np
 from random import random
-from time import time
+from time import time, sleep
 pg.font.init()
-font = pg.font.SysFont('Comic Sans', 30)  # cus it's gotta be Comic Sans
 
 # constants:
-CELL_SIZE = 5  # size of each cell (CHANGE THIS ONE)
-SCREEN_SIZE = 800  # n*n pixel window
+CELL_SIZE = 50  # size of each cell (CHANGE THIS ONE)
+SCREEN_SIZE = 1000  # n*n pixel window
 ARRAY_SIZE = int(SCREEN_SIZE // CELL_SIZE)  # size of the array, based on the scale factor
 LIVE_CELL_COLOUR = (0, 190, 0)  # (R, G, B)
 CELL_GEN_CHANCE = 0.5  # chance of cell being alive
-MAX_FPS = 10  # fps limit (set to 0 for no limit)
+MAX_FPS = 0  # fps limit (set to 0 for no limit)
+ERASER_SIZE = 5  # n*n cells eraser size
 
 # rule types:
 classic = (3, 2, 3)
@@ -26,14 +26,13 @@ rule_type = classic
 INT_RGB = sum(LIVE_CELL_COLOUR[2-i] * (256 ** i) for i in range(3))
 arr = np.array([[0] * ARRAY_SIZE] * ARRAY_SIZE, dtype=int)  # main array; 0 - dead cell, 1 - live cell
 all_positions = [(y, x) for y in range(ARRAY_SIZE) for x in range(ARRAY_SIZE)]  # array of all positions
-offsets = np.array([[i, j] for i in range(-1, 2) for j in range(-1, 2) if not (i == j == 0)])
+offsets = np.array([[i, j] for i in range(-1, 2) for j in range(-1, 2)])
 
 ttime = -1
 time_taken = 0
 fps = 0
-
-running = True
 paused = False
+font = pg.font.SysFont('Comic Sans', 30)  # cus it's gotta be Comic Sans
 
 # set up pygame:
 window = pg.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
@@ -44,21 +43,21 @@ window.fill(0)
 # functions
 
 
-def lower_limit(value):
-    if value < 0: return 0
+def hard_limit(value: int, lower: int = 0, upper: int = ARRAY_SIZE - 1) -> int:
+    if value < lower: return lower
+    elif value > upper: return upper
     else: return value
 
 
-def upper_limit(value, limit):
-    if value > limit: return limit
-    else: return value
+def numpy_wrap(o: int, ax: int, array):
+    return array.take(range(-1+o, 2+o), mode='wrap', axis=ax)
 
 
-def clear_game_array():
+def clear_game_array() -> None:
     arr[:] = 0
 
 
-def randomize_game_array():
+def randomize_game_array() -> None:
     clear_game_array()
     for j in range(ARRAY_SIZE):
         for i in range(ARRAY_SIZE):
@@ -70,7 +69,7 @@ def randomize_game_array():
 
 print("running")
 t = time()
-while running:
+while True:
     # check if game-window is closed
     if pg.QUIT in [event.type for event in pg.event.get()]: break
 
@@ -78,10 +77,12 @@ while running:
     # keyboard inputs
 
     keys = pg.key.get_pressed()  # get the state of all keys
-    paused = keys[pg.K_p]                    # p   -> paused game
     if keys[pg.K_c]: clear_game_array()      # c   -> clear array
     if keys[pg.K_r]: randomize_game_array()  # r   -> randomize array
     if keys[pg.K_ESCAPE]: break              # esc -> exit game
+    if keys[pg.K_p]:                         # p   -> pause/unpause game
+        paused = not paused
+        sleep(0.1)
 
     ################################################
     # mouse inputs
@@ -89,22 +90,22 @@ while running:
     mouse_buttons = pg.mouse.get_pressed()
     if True in mouse_buttons:
         mouse_pos = pg.mouse.get_pos()
-        posX = int(mouse_pos[1] / CELL_SIZE)
-        posY = int(mouse_pos[0] / CELL_SIZE)
+        posX = mouse_pos[1] // CELL_SIZE
+        posY = mouse_pos[0] // CELL_SIZE
+
+        if not (0 <= posX < ARRAY_SIZE and 0 <= posY < ARRAY_SIZE): continue
 
         if mouse_buttons[0]:  # for creating live cells (left click)
             arr[posY, posX] = 1
-
         elif mouse_buttons[2]:  # for deleting live cells (right click)
             arr[posY, posX] = 0
-
         elif mouse_buttons[1]:  # for big eraser (middle click)
-            length = int(ARRAY_SIZE / 5)
+            length = int(ARRAY_SIZE / ERASER_SIZE)
 
-            x1, x2 = lower_limit(posX - length // 2), upper_limit(posX + round(length / 2), ARRAY_SIZE)
-            y1, y2 = lower_limit(posY - length // 2), upper_limit(posY + round(length / 2), ARRAY_SIZE)
+            x1 = hard_limit(posX - length // 2)
+            y1 = hard_limit(posY - length // 2)
 
-            arr[y1:y2, x1:x2] = 0
+            arr[y1:y1+length, x1:x1+length] = 0
 
     ################################################
     # play mode
@@ -124,39 +125,39 @@ while running:
         # (I chose the values through some experimentation with the rule types)
         to_check = []
         if ones_percentage < (0.14 - 0.015 * (rule_type[2] - rule_type[1] + 1)):
-            to_check = []
             for offset in offsets:
                 to_check.extend(np.add(ones, [offset] * ones_len))
-                # (check for valid coordinates of each offset cell is performed later)
+            # (wrapping for invalid positions is preformed later)
 
-            # adding the live sells themselves (the centers)
-            to_check.extend(ones)
             # removing duplicate cell positions
-            to_check = list(set(map(lambda k: tuple(k), to_check)))
+            to_check = list(set(map(tuple, to_check)))
 
         ################################################
 
         # creating a copy of the game array and changing the main array
         arr_c = np.copy(arr)  # copying array
         for posY, posX in (to_check or all_positions):
-            # making sure cell isn't off-screen:
-            if not (0 <= posX <= ARRAY_SIZE - 1 and 0 <= posY <= ARRAY_SIZE - 1): continue
+            # wrap coords:
+            posX %= ARRAY_SIZE
+            posY %= ARRAY_SIZE
 
             # getting number of neighbours:
-            adjacent_cells_arr = arr_c[lower_limit(posY-1):posY + 2, lower_limit(posX-1):posX + 2]
-            neighbours = int(np.count_nonzero(adjacent_cells_arr))  # number of non-zero values in grid
-            # (1 extra neighbour if current cell is live, accounted for later)
+            #adjacent_cells_arr = arr_c.take(range(-1+posY, 2+posY), mode='wrap', axis=0).take(range(-1+posX, 2+posX), mode='wrap', axis=1)
+            adjacent_cells_arr = numpy_wrap(posX, 1, numpy_wrap(posY, 0, arr_c))
+            neighbours = int(np.count_nonzero(adjacent_cells_arr)) - int(arr_c[posY, posX] == 1)
+            # (subtract 1 if current cell is alive)
 
             # applying the rules:
-
-            # dead cell -> live cell, if it has 3 neighbours
+            # dead cell -> live cell, if it has rule_type[0] (default=3) neighbours
             if arr_c[posY, posX] == 0 and neighbours == rule_type[0]:
                 arr[posY, posX] = 1
 
-            # live cells -> dead cells, if it doesn't have 2 to 3 neighbors
-            if arr_c[posY, posX] == 1 and not (rule_type[1] <= neighbours - 1 <= rule_type[2]):
+            # live cells -> dead cells, if it doesn't have
+            # from [rule_type[1] (default=2)] to [rule_type[2] (default=2)] neighbors
+            if arr_c[posY, posX] == 1 and not (rule_type[1] <= neighbours <= rule_type[2]):
                 arr[posY, posX] = 0
 
+        # fps calculations:
         time_taken = time() - t
         if time_taken != 0:
             fps = round(1/time_taken, 2)
@@ -166,7 +167,7 @@ while running:
     # update the screen
     pg.surfarray.blit_array(window, np.kron(arr, np.full((CELL_SIZE, CELL_SIZE), INT_RGB)))
 
-    text_surface = font.render(f"FPS: {fps}", False, (255, 255, 255))
+    text_surface = font.render(f"FPS: {fps} {'(paused)' if paused else ''}", False, (255, 255, 255))
     window.blit(text_surface, (0, 0))
 
     pg.display.update()
